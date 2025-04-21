1c6235b374f48bbf2bc04e24aecc23c715e410f1
Mostafa Rayhan <mostafarayhan57@gmail.com>
2025-04-21 23:54:04 +0600

    create DTOs

--- API/Models/AuthModels.cs
+++ API/Models/AuthModels.cs
@@ -1,6 +1,38 @@
 ﻿namespace API.Models
 {
-    public class AuthModels
+    public class RegisterModel
     {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+
+    public class LoginModel
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+
+    public class ChangePasswordModel
+    {
+        public string? Email { get; set; }
+        public string? CurrentPassword { get; set; }
+        public string? NewPassword { get; set; }
+    }
+
+    public class ResetPasswordModel
+    {
+        public string? Email { get; set; }
+        public string? Token { get; set; }
+        public string? NewPassword { get; set; }
+    }
+
+    public class AuthResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+        public string? Token { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? Expiration { get; set; }
     }
 }
