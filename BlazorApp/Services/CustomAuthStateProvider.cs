6920cebda1c61c00c265372ed8781352013b17fa
Mostafa Rayhan <mostafarayhan57@gmail.com>
2025-04-23 02:26:00 +0600

    Create AuthenticationStateProvider

---
+++ BlazorApp/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,37 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorApp.Services
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var state = new AuthenticationState(new ClaimsPrincipal());
+
+            string token = await _localStorage.GetItemAsStringAsync("token");
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "user"),
+                }, "test authentication type");
+
+                state = new AuthenticationState(new ClaimsPrincipal(identity));
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+    }
+}
